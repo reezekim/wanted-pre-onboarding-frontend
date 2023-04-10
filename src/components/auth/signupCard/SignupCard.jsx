23ecd6321b80862e574dbc96901137811660219e
evelyn <evelynkim.kr@gmail.com>
2023-04-10 15:39:27 +0900

    Feat: Add component-Signup Form, Input and api-signupRequest to SignupCard.jsx

---
+++ src/components/auth/signupCard/SignupCard.jsx
@@ -0,0 +1,75 @@
+import React, { useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signupRequest } from "../../../apis/signup";
+import styles from "./SignupCard.module.css";
+
+export default function SignupCard() {
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({ email: "", password: "" });
+
+  const goToLogin = () => {
+    navigate(`/signin`);
+  };
+
+  const validation = !(
+    inputValue.email.includes("@") && inputValue.password.length >= 8
+  );
+
+  const handleChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setInputValue({ ...inputValue, [name]: value });
+      e.preventDefault();
+    },
+    [inputValue]
+  );
+
+  const signupSubmit = (e) => {
+    const email = inputValue.email;
+    const password = inputValue.password;
+    e.preventDefault();
+    signupRequest(email, password);
+  };
+
+  return (
+    <form onSubmit={signupSubmit}>
+      <h2 className={styles.title}>Sign Up</h2>
+
+      <input
+        data-testid="email-input"
+        type="email"
+        name="email"
+        placeholder="이메일"
+        value={inputValue.email}
+        onChange={handleChange}
+      />
+      <input
+        data-testid="password-input"
+        type="password"
+        name="password"
+        placeholder="비밀번호"
+        value={inputValue.password}
+        onChange={handleChange}
+      />
+      <button
+        data-testid="signup-button"
+        type="submit"
+        disabled={validation}
+        className={styles.btn}
+      >
+        회원가입
+      </button>
+      <div className={styles.desc}>
+        <p className={styles.sub}>이미 회원이신가요?</p>
+        <button
+          data-testid="signin-button"
+          type="button"
+          onClick={goToLogin}
+          className={styles.link}
+        >
+          로그인
+        </button>
+      </div>
+    </form>
+  );
+}
