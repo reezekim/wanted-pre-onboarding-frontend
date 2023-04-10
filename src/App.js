f3199a7d221cc9fb94b37ab570cee59dd341ecd9
evelyn <evelynkim.kr@gmail.com>
2023-04-10 14:50:46 +0900

    Add: component-Footer to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { Outlet } from 'react-router-dom';
 import './App.css';
+import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <Outlet />
+      <>
+        <Outlet />
+        <Footer />
+      </>
   );
 }
 
