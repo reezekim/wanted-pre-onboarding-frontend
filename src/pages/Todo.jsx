df3bf8ffd41d0fea00e9698d17e52920bf3f95af
evelyn <evelynkim.kr@gmail.com>
2023-04-11 22:00:40 +0900

    Style: Add css-section to Todo.jsx

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import TodoList from "../components/todo/todoList/TodoList";
+import styles from "../css/section.module.css";
 
 export default function Todo() {
   return (
-    <>
+    <section className={styles.section}>
       <TodoList />
-    </>
+    </section>
   );
 }
