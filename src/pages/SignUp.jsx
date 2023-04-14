cb0893fad12b0381266748d4e8dd86e16bd7c61e
evelyn <evelynkim.kr@gmail.com>
2023-04-14 23:55:53 +0900

    Feat: Add navigate to pages-SignUp

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SignupCard from "../components/auth/signupCard/SignupCard";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      navigate(`/signin`);
+    }
+  }, [navigate]);
+
   return (
     <section>
       <SignupCard />
