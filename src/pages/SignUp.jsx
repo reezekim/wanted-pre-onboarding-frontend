9e2f27477f7577bce1c10dc5b186a067ecd034bd
evelyn <evelynkim.kr@gmail.com>
2023-04-14 23:17:53 +0900

    Fix: deleted CSS-section

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import SignupCard from "../components/auth/signupCard/SignupCard";
-import styles from "../css/section.module.css";
 
 export default function SignUp() {
   return (
-    <section className={styles.section}>
+    <section>
       <SignupCard />
     </section>
   );
