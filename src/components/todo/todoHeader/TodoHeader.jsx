3af575fea5047f6349d041d7271096581c2fa07a
evelyn <evelynkim.kr@gmail.com>
2023-04-19 23:39:57 +0900

    Fix: Style text

--- src/components/todo/todoHeader/TodoHeader.jsx
+++ src/components/todo/todoHeader/TodoHeader.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./TodoHeader.module.css";
 import { useNavigate } from "react-router-dom";
-import { RiLogoutCircleRLine } from "react-icons/ri";
 
 export default function TodoHeader() {
   const navigate = useNavigate();
@@ -16,7 +15,7 @@ export default function TodoHeader() {
       <div className={styles.cont}>
         <h2 className={styles.title}> To Do List</h2>
         <button type="button" onClick={handleSignout} className={styles.toggle}>
-          <RiLogoutCircleRLine className={styles.icon} />
+          Logout
         </button>
       </div>
     </header>
